983b4bd3601976ad20605bd1d8f266288455e7d9
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-06-15 05:22:24 +0000

    Done

---
+++ Loops/multiplicationTable.java
@@ -0,0 +1,15 @@
+public class multiplicationTable {
+
+    public static void main(String[] args) {
+
+        int table = 5;
+
+        int i = 1;
+        while ( i <= 10 ) {
+
+            System.out.println(" 5 " + " * " +  i  +" = " + table * i);
+            i++;
+
+        }
+    }
+}
